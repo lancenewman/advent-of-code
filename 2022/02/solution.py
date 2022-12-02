9f070cf4c8bf502aedb1afa02084d6f80a26976c
Lance Newman <lance.newman.642@gmail.com>
2022-12-02 08:59:20 -0500

    Add day 2 solution & tests

---
+++ 2022/02/solution.py
@@ -0,0 +1,73 @@
+def evaluate_round_part_1(elf_move, my_move):
+    lose_conditions = {
+        "A": "Z",
+        "B": "X",
+        "C": "Y"
+    }
+
+    draw_conditions = {
+        "A": "X",
+        "B": "Y",
+        "C": "Z",
+    }
+
+    move_values = {
+        "X": 1,
+        "Y": 2,
+        "Z": 3,
+    }
+
+    my_score = move_values[my_move]
+
+    if lose_conditions[elf_move] == my_move:
+        return my_score
+    elif draw_conditions[elf_move] == my_move:
+        return my_score + 3
+    else:
+        return my_score + 6
+
+def evaluate_round_part_2(elf_move, outcome):
+    LOSE = "X"
+    DRAW = "Y"
+    WIN = "Z"
+
+    move_values = {
+        "A": 1,
+        "B": 2,
+        "C": 3
+    }
+
+    win_map = {
+        "A": move_values["B"],
+        "B": move_values["C"],
+        "C": move_values["A"]
+    }
+
+    lose_map = {
+        "A": move_values["C"],
+        "B": move_values["A"],
+        "C": move_values["B"]
+    }
+
+    if outcome == LOSE:
+        return lose_map[elf_move]
+    elif outcome == DRAW:
+        return move_values[elf_move] + 3
+    else:
+        return win_map[elf_move] + 6
+
+def solve(file):
+    part_1_score = 0
+    part_2_score = 0
+    with open(file) as input:
+        for line in input:
+            elf_score, my_score = line.split()
+            part_1_score = part_1_score + evaluate_round_part_1(elf_score, my_score)
+            part_2_score = part_2_score + evaluate_round_part_2(elf_score, my_score)
+    return (part_1_score, part_2_score)
+
+
+if __name__ == '__main__':
+    part1, part2 = solve('input.txt')
+    print(f'Part 1: {part1}')
+    print(f'Part 2: {part2}')
---
+++ 2022/02/test_solution.py
@@ -0,0 +1,44 @@
+import unittest
+import solution
+
+class TestEvaluateRoundPart1(unittest.TestCase):
+    
+    def test_win(self):
+        elf_move = "A"
+        my_move = "Y"
+        self.assertEqual(8, solution.evaluate_round_part_1(elf_move, my_move))
+
+    def test_loss(self):
+        elf_move = "B"
+        my_move = "X"
+        self.assertEqual(1, solution.evaluate_round_part_1(elf_move, my_move))
+
+    def test_draw(self):
+        elf_move = "C"
+        my_move = "Z"
+        self.assertEqual(6, solution.evaluate_round_part_1(elf_move, my_move))
+
+
+class TestEvaluateRoundPart2(unittest.TestCase):
+
+    def test_win(self):
+        elf_move = "C"
+        outcome = "Z"
+        self.assertEqual(7, solution.evaluate_round_part_2(elf_move, outcome))
+
+    def test_draw(self):
+        elf_move = "A"
+        outcome = "Y"
+        self.assertEqual(4, solution.evaluate_round_part_2(elf_move, outcome))
+
+    def test_lose(self):
+        elf_move = "B"
+        outcome = "X"
+        self.assertEqual(1, solution.evaluate_round_part_2(elf_move, outcome))
+
+
+class TestSolve(unittest.TestCase):
+
+    def test_file(self):
+        file = "test.txt"
+        self.assertEqual((15, 12), solution.solve(file))
