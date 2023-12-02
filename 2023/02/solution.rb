e435190f944fdbb2a97b01aa220613f5c007f7bc
Lance Newman <lance.newman.642@gmail.com>
2023-12-02 06:52:59 -0500

    Clarify vocabulary

--- 2023/02/solution.rb
+++ 2023/02/solution.rb
@@ -1,3 +1,9 @@
+# Processing hierarchy
+# Line: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+# Game: "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+# Handfull: "3 blue, 4 red"
+# Dice: { red: 4, green: 0, blue; 3 }
+
 DICE_LIMITS = {
   red: 12,
   green: 13,
@@ -9,18 +15,18 @@ def get_id(line)
   line.split(" ", 2).last.chomp(":").to_i
 end
 
-def get_handfulls(line)
+def get_game(line)
   line.split(":", 2)[1].split(";")
 end
 
-def get_dice(handfull)
+def get_dice(game)
   res = {
     red: 0,
     green: 0,
     blue: 0,
   }
 
-  handfull.split(",").each do |dice| 
+  game.split(",").each do |dice| 
     amount, color = dice.split
     res[color.to_sym] = amount.to_i
   end
@@ -28,21 +34,27 @@ def get_dice(handfull)
   res
 end
 
-def valid_game?(dice)
-  dice.all? do |handfull|
-    handfull[:red] <= DICE_LIMITS[:red] && 
-    handfull[:green] <= DICE_LIMITS[:green] && 
-    handfull[:blue] <= DICE_LIMITS[:blue]
+def valid_selection?(handfull)
+  handfull.all? do |dice|
+    dice[:red] <= DICE_LIMITS[:red] && 
+    dice[:green] <= DICE_LIMITS[:green] && 
+    dice[:blue] <= DICE_LIMITS[:blue]
+  end
+end
+
+def min_dice(dice)
+  dice.each do |handfull|
+
   end
 end
 
 input = File.readlines("input.txt")
 sum = 0
 input.each do |line|
-  handfulls = get_handfulls(line)
-  dice = handfulls.map { |handfull| get_dice(handfull) }
-  puts dice
-  if valid_game?(dice)
+  game = get_game(line)
+  handfulls = game.map { |handfull| get_dice(handfull) }
+  puts handfulls
+  if valid_selection?(handfulls)
     sum = sum + get_id(line)
   end
 end
